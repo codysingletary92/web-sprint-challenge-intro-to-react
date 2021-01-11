538d071c2ea6de285b053fc62ca1bba499bed54c
samuel singletary <codysingletary92@gmail.com>
2021-01-11 01:46:35 -0500

    ugh.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Characters data={dummyData}/>
+    <Characters />
   );
 }
 
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,35 +1,39 @@
 import React, {useState} from 'react'
 import Character from './Character.js'
+import axios from 'axios'
 
 
 const Characters = props => {
 
-const characterList = [];
+const {chars, setChars} = useState([])
 
-// axios.get('https://swapi.py4e.com/api/people')
-//     .then( result => {
-//       const {data} = result;
-//         let i;
+axios.get('https://swapi.py4e.com/api/people')
+    .then( result => {
+      const {data} = result;
+        let i;
 
-//         function get(resultData) {
-//             characterList.push(resultData)   
-//         }
+        function get(resultData) {
+            setChars('asdasd')
+            console.log(chars)   
+        }
 
-//         for (i=1; i <= data.count ;i++){
-//             axios.get(`https://swapi.py4e.com/api/people/${i}/`)
-//                 .then(result => get(result.data))
-//                 .catch(err => console.log(err))
-//             //console.log(characterList)
-//         }
-//     })
-//     .catch(err => console.log(err)) ;
+        for (i=1; i <= data.count ;i++){
+            axios.get(`https://swapi.py4e.com/api/people/${i}/`)
+                .then(result => get(result.data))
+                .catch(err => console.log(err))
+            //console.log(characterList)
+        }
+    })
+    .catch(err => console.log(err)) ;
 
-const list = props.data.results;
-console.log(list)
+
+
+// const list = props.data.results;
+// console.log(list.map( obj => obj))
 
 return (
        <div>
-           {list.map( (item, index) => <Character key={index} data={item} />)}
+           {/* {list.map( (item, index) => <Character key={index} data={item} />)} */}
        </div>
         // (props.data).map( (item, index) => <Character key={index} data={item} />)
     
